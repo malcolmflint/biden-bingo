7e7ae745f4dd24f19a9f3172143582f3a03ddc14
Malcolm Flint <malcolm.flint@gmail.com>
2021-01-31 15:03:13 -0800

    Creates random bingo cards now!

--- bingo.js
+++ bingo.js
@@ -20,32 +20,16 @@ svg.append('svg:image')
     .attr('width', width)
     .attr('height', height);
 
-d3.csv('demands.csv', function (d) {
-    return d.demand;
-}).then(function(data) {
-    console.log(data);
-    let bingoSquares = Array();
-    for (let x = 0; x < 5; x++) {
-        for (let y = 0; y < 5; y++) {
-            let demand = '';
-            if (x === 2 && y === 2) {
-                demand = 'Free Space!';
-            } else {
-                const demandNum = Math.floor(Math.random() * data.length);
-                // demand = data.splice(demandNum, 1);
-                demand = demandNum;
-            }
-            let demandBox = svg.append('g');
 
-            demandBox.append('text')
-                .attr('x', gridX + gridScale(x + 0.5))
-                .attr('y', gridY + gridScale(y + 0.5))
-                .attr('dominant-baseline', 'middle')
-                .attr('text-anchor', 'middle')
-                .text(demand);
-        }
+const numEntries = 91;
+for (let x = 0; x < 5; x++) {
+    for (let y = 0; y < 5; y++) {
+        const demandNum = Math.floor(Math.random() * numEntries);
+        const demandPath = `squares/bingo_squares${demandNum}.png`;
+
+        svg.append('svg:image')
+            .attr('xlink:href', demandPath)
+            .attr('x', gridX + gridScale(x))
+            .attr('y', gridY + gridScale(y));
     }
-    // d3.selectAll('text').data(bingoSquares).enter()
-    //     .append('text')
-    //     .
-});
+}
